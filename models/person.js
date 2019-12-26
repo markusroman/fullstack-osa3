445850ff207bf420af56c709d1038fe6ce55000f
Markus Röman <markus.roman@hotmail.fi>
2019-12-26 11:16:26 +0200

    'uibuild'

--- models/person.js
+++ models/person.js
@@ -16,7 +16,7 @@ const personSchema = new mongoose.Schema({
     number: String,
 })
 
-noteSchema.set('toJSON', {
+personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
@@ -24,4 +24,4 @@ noteSchema.set('toJSON', {
     }
 })
 
-module.exports = mongoose.model('Note', personSchema)
+module.exports = mongoose.model('Person', personSchema)
