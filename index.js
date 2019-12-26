2c1711472ced89cc1716dd9439d0d9655fcb274a
Markus Röman <markus.roman@hotmail.fi>
2019-12-26 11:00:07 +0200

    'uibuild'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require('mongoose')
 const bodyParser = require("body-parser");
 const Person = require('./models/person')
 
