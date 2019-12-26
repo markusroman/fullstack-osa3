d67da6bda3bc868f086fb6309613881f59d765b7
Markus Röman <markus.roman@hotmail.fi>
2019-12-26 18:12:27 +0200

    'uibuild'

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.put("/api/persons/:id", (req, res, next) => {
 app.delete("/api/persons/:id", (req, res) => {
   Person.findOneAndDelete({ name: req.params.id })
     .then(() => {
-      response.status(204).end()
+      res.status(204).end()
     })
     .catch(error => next(error))
 });
